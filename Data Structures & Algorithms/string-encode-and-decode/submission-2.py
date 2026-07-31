64d065d40ec549407cd28df3e28403254ff51463
shauryaneeraj-123 <shauryaneeraj5@gmail.com>
2026-08-01 00:07:56 +0100

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        #analogy: speech to print
+        parts = []
+
+        for s in strs:
+            parts.append(f"{len(s)}#{s}")
+
+        return "".join(parts)
+
+    def decode(self, s: str) -> List[str]:
+        #analogy: print to speech 
+        parts = []
+        i = 0
+
+        while i < len(s):
+            j = i
+
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[i:j])
+
+            start = j + 1
+            end = start + length
+
+            parts.append(s[start:end])
+
+            i = end
+
+        return parts
