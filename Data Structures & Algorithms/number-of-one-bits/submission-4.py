fb078a29206cf88490ab6762e074b6c28b305d0b
shauryaneeraj-123 <shauryaneeraj5@gmail.com>
2026-08-09 16:42:41 +0200

    Add: number-of-one-bits - submission-4

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+
+        while n:
+            # n & (n - 1) removes the RIGHTMOST 1-bit from n
+            n &= n - 1
+
+            # Since we removed exactly one 1-bit,
+            # increase our count by 1
+            count += 1
+
+        return count
